4aff74e6ad21841ee6b3e5f5bf0a2cfc2873dbcf
diegosales <diegofelipesales23@gmail.com>
2023-05-03 16:18:09 -0400

    finalizando menu mobile first

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,12 @@ import React, { useState, useEffect } from "react";
 import Menu from "./Menu";
 import Wrapper from "./Wrapper";
 
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
+import MenuMobile from "./MenuMobile";
+
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [showCatMenu, setShowCatMenu] = useState(false);
@@ -16,10 +22,51 @@ const Header = () => {
     >
       <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
-            <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]"/>
-          
+          <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]" />
         </Link>
-        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+
+        {
+            mobileMenu && 
+            <MenuMobile showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} setMobileMenu={setMobileMenu}/>
+        }
+        <div className="flex items-center gap-2 text-black">
+        {/* Icon favorito start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[19px]  md:text-[24px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute
+                 top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] "
+            >
+              51
+            </div>
+          </div>
+        {/* Icon favoritos final */}
+
+        {/* Icon cart start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px]  md:text-[20px]" />
+            <div
+              className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute
+                 top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px] "
+            >
+              5
+            </div>
+          </div>
+        {/* Icon cart final */}
+
+        {/* Mobile Icon inicio */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -mr-2">
+            {mobileMenu ? (
+                <VscChromeClose className="text-[16px] " onClick={() => setMobileMenu(false)} />
+            ) : (
+                <BiMenuAltRight className="text-[20px] " onClick={() => setMobileMenu(true)} />
+            )}
+          </div>
+          {/* Mobile Icon final */}
+
+
+        </div>
       </Wrapper>
     </header>
   );
