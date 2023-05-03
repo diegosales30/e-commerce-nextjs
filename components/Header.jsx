55a1a23e5e5179bc372ac062bc4fc01b843a91c0
diegosales <diegofelipesales23@gmail.com>
2023-05-03 03:20:04 -0400

    construindo o header com tailwind

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import Menu from "./Menu";
+import Wrapper from "./Wrapper";
 
 const Header = () => {
-    return <div className="text">Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShowu] = useState("translate-y-0");
+  const [lastScrolly, setLastScrolly] = useState(0);
+
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper className="h-[60px] flex justify-between items-center">
+        <Link href="/">
+            <img src="/logo.svg" alt="logo" className="w-[40px] md:w-[60px]"/>
+          
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu}/>
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
